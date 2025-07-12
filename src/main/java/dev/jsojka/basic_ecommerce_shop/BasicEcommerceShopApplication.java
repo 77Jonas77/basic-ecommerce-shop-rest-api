f2661e545d4915a16fa72cb97f647dfdb1dfc8a3
s27523 <s27523@pjwstk.edu.pl>
2025-07-12 19:44:53 +0200

    Initial commit

---
+++ src/main/java/dev/jsojka/basic_ecommerce_shop/BasicEcommerceShopApplication.java
@@ -0,0 +1,13 @@
+package dev.jsojka.basic_ecommerce_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicEcommerceShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicEcommerceShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jsojka/basic_ecommerce_shop/BasicEcommerceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jsojka.basic_ecommerce_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicEcommerceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
